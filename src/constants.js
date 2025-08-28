a4018cfa9dcfffe9c2aa5791416567fc5a5fe6b2
Birjesh0000 <gbirjesh0000@gmail.com>
2025-08-28 21:29:54 +0530

    initilized backend

---
+++ src/constants.js
@@ -0,0 +1,2 @@
+// this file contains all the constants used in the project, like name of database
+export const DB_NAME = "blogApp";
---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+// here we write logic to connect to database
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+export const connectDB = async () => {  /// use of async await bcz db is in another continent
+    try {
+        const connection = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`); // connecting to database
+        console.log("Database connected");
+    } catch (error) {
+        console.log("thers is connection error", error);
+        process.exit(1);// exit the process if there is error
+    }
+};
