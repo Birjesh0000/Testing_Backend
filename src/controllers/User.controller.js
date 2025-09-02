6cac53192347cf1258223ebd2bfa33580c8b0ace
Birjesh0000 <gbirjesh0000@gmail.com>
2025-09-02 16:02:26 +0530

    user register logic implemented

--- src/controllers/User.controller.js
+++ src/controllers/User.controller.js
@@ -1,9 +1,86 @@
 import {AsyncHandler} from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js";
+import {User} from "../models/User.model.js";
+import {uploadToCloudinary} from "../utils/Cloudinary.js";
+import {ApiResponse} from "../utils/ApiResponse.js";
+
 
 const registerUser = AsyncHandler(async (req, res) => {
-    res.status(201).json({
-         message: "User registered successfully" 
-    });
+    // Extract user details from request body
+    //perform validation
+    //check if user already exists
+    // check for necessary fields, like name, email, password, avatar
+    // upload images on cloudinary
+    // create user object -> create entry of new user in the database
+    /// remove sensitive info like password and refresh token field from response
+    /// check for user creation success
+    // send response to client
+
+    /// if data from frontend as form data or json data, then it will be in req.body
+    /// otherwise if data is sent via url, then it will be in req.query
+    const {fullName, email, userName, avatar, password} = req.body;
+
+    // now checks for necessary fields using multiple if conditions
+    // if(fullName === ""){
+    //     throw new ApiError(400, "Full name is required");
+    // }
+
+    // or use single if condition, which is more efficient
+    if(
+        [fullName, email, userName, avatar, password].some((field) => !field || field.trim() === "")
+    ){
+        throw new ApiError(400, "All fields are required");
+    }
+
+    // upper check is for empty string, null, undefined, or string with only spaces
+    // further validations can be added, like email format, password strength, etc.
+
+    // now check if user already exists with the same email and username
+    const existingUser = await User.findOne({$or: [{ email }, { userName }]});
+    if(existingUser){
+        throw new ApiError(409, "User already exists with this email or username");
+    }
+
+    // if avatar is a file, then it will be in req.files (handled by multer middleware)
+    // if avatar is a url or base64 string, then it will be in req.body
+    // but we assume avatar is a file ( as already handles by multer middleware in router files)
+
+    const avatarLocalPath = req.files?.avatar[0]?.path; // get the path of uploaded avatar image
+    const coverImageLocalPath = req.files?.coverImage[0]?.path; // get the path of uploaded cover image
+
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Avatar Image are required");
+    }
+    
+    // upload avatar to cloudinary
+    const avatarImage = await uploadToCloudinary(avatarLocalPath);
+    const coverImage = coverImageLocalPath ? await uploadToCloudinary(coverImageLocalPath) : null;
+
+    if(!avatarUploadResponse){
+        throw new ApiError(500, "Failed to upload avatar to cloudinary");
+    }
+
+    // create user object, for database entry
+    const User = await User.create({
+        fullName,
+        avatar: avatarImage.url,
+        converImage: coverImage ? coverImage.url : undefined,
+        email,
+        userName: userName.toLowerCase(), // to make username case-insensitive
+        password // will be hashed in pre-save hook of User model
+    })
+
+    // check if user creation was successful
+    const createdUser = await User.findById(User._id).select("-password -refreshToken -__v -createdAt -updatedAt");
+    if(!createdUser){
+        throw new ApiError(500, "User registration failed, please try again");
+    }
+
+    // 
+    return res.status(201).json(
+        new ApiResponse(200, "User registered successfully", createdUser)
+    );
+
 });
 
 export {registerUser};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,18 @@
 import {Router} from "express";
 import { registerUser } from "../controllers/User.controller.js";
+import {uplaod} from "../middlewares/multer.js";
 
 const router = Router(); // Create a new router instance
 
-router.route("/register").post(registerUser); // Define a POST route for user registration
-//router.route("/login").post(loginUser);       // Define a POST route for user login
+// handover all the requests on this route to the controller (registerUser function)
+// use of middleware (upload) to handle file uploads, before reaching the controller
+router.route("/register").post(
+    uplaod.fields(
+        {name: "avatar", maxCount: 1},
+        {name: coverImage, maxCount: 1}
+    ), 
+    registerUser
+);
 
 
 export default router;
